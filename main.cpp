d7443795a9adcfed54dbe53bb84e4622908906da
Michael <michael90@protonmail.com>
2025-10-11 18:21:36 +0200

    Improve logging to track down advertising error

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
+#include <QLoggingCategory>
 #include "ftmsdevice.h"
 
 int main(int argc, char *argv[])
 {
+    QLoggingCategory::setFilterRules(
+        "qt.bluetooth*.debug=true\n"
+        "qt.bluetooth*.info=true\n"
+        "qt.bluetooth.bluez*.debug=true\n"
+    );
     QCoreApplication a(argc, argv);
 
     FTMSDevice *ftms = new FTMSDevice(100);
